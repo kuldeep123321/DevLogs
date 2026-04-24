889d92328e350f93520f0284df5f0eff42f325b8
kuldeep <kuldeepsinghrawat123321@gmail.com>
2026-04-24 16:10:26 +0530

    Flow Structure

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Clientr from './ROLES/Clientr'
-import Signup from './Components/Authentication/SignUp'
+// import Signup from './Components/Authentication/SignUp'
 import Login from './Components/Authentication/Login'
 function App() {
   return (
